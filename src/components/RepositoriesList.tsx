122e62cccc50d9f57e86a127b6e96437a7e074a1
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-27 01:06:57 -0300

    Add repositories list and useTypedSelector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { loading, error, data } = useTypedSelector(
+    (state) => state.repositories
+  );
+  console.log(loading, error, data);
+  const { searchRepositories } = useActions();
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
