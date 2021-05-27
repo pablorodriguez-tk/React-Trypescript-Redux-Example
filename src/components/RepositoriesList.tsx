2cfafd511f6941bfcb1b15b3d4c0526a80a6ee98
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-27 01:17:32 -0300

    Add data.map

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -7,7 +7,6 @@ const RepositoriesList: React.FC = () => {
   const { loading, error, data } = useTypedSelector(
     (state) => state.repositories
   );
-  console.log(loading, error, data);
   const { searchRepositories } = useActions();
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -20,6 +19,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
